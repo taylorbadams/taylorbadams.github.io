20a2a24be64014dfcef4153c3f43128cc2732af4
Brandon Martinez <bmartheswe@gmail.com>
2026-06-28 00:26:36 -0500

    using template for creating nav bar

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,18 @@
-const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy({
-			"./assets/": "/assets/",
-		})
+    "./assets/": "/assets/",
+  })
+
+  return {
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "liquid", // Forces .html files to use Liquid
+    templateFormats: ["html", "md", "liquid"], // Explicitly tracks these extensions
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
 };
