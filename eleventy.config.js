97b6f8fe29a3bea4a1c39fe6c648b7cdd2f50845
Brandon Martinez <bmartheswe@gmail.com>
2026-06-29 18:36:50 -0500

    adding CNAME

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy({
     "./assets/": "/assets/",
+    "./CNAME": "/CNAME",
   })
 
   return {
